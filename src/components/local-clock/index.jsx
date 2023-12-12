ed460e353b8a96978ed75112df79e6c063903599
MaHi <muhammadshefain123@gmail.com>
2023-12-12 23:58:52 +0600

    doing some test

--- src/components/local-clock/index.jsx
+++ src/components/local-clock/index.jsx
@@ -1,25 +1,26 @@
-import useClock from '../../hooks/useClock';
+import  useClock  from '../../hooks/useClock';
 
 function ClockList() {
   const { clock: local } = useClock();
   const { clock: est } = useClock('EST');
   const { clock: pst } = useClock('PST');
-  const { clock: pakistan } = useClock('UTC', 5 * 60);
   const { clock: edt } = useClock('EDT');
   const { clock: british } = useClock('BST');
   const { clock: mst } = useClock('MST');
+  const { clock: nepal } = useClock('UTC', (60 * 5) + 45);
 
   console.log('local', local.date);
   console.log('est', est.date);
   console.log('pst', pst.date);
-  console.log('pakistan', pakistan.date);
   console.log('edt', edt.date);
   console.log('british', british.date);
   console.log('mst', mst.date);
+  console.log('Nepal', nepal.date);
 
   return (
     <div>
-      <h1> Local Clock</h1>
+      <h1>Local Clock</h1>
+      {/* Render other clock components or their information as needed */}
     </div>
   );
 }
